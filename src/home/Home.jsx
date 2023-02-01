a6b970527ee588c58bc05841cd4be52caba8c3da
anshulamrahs <anshulamrahs@gmail.com>
2023-02-01 22:11:19 +0530

    new home additions

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,8 +1,10 @@
 import "./home.scss";
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 const Hello = () => {
   return (
     <div className='home'>
+        <AcUnitIcon/>
         Hello
     </div>
   )
