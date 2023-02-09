741ad60e2918e2c7af0f2adcc2dae1a6e910b104
anshulamrahs <anshulamrahs@gmail.com>
2023-02-09 09:19:46 +0530

    The UI completion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./app.scss"
 import Home from "./pages/home/Home";
-import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 
 function App() {
-    return <Watch/>
+    return <Home/>
   }
   
   export default App;
